44d27504d85d8a62504cce0c4f1dfa07cd0ad567
Laurkan <laurkanr@gmail.com>
2026-07-31 18:25:25 -0400

    adding the admin

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Deadline, DeadlineScheduleItemStatus, DeadlineScheduleItemType, Event, EventScheduleItemStatus, EventScheduleItemType
+
 # Register your models here.
+admin.register(EventScheduleItemStatus)
+admin.register(DeadlineScheduleItemStatus)
+admin.register(EventScheduleItemType)
+admin.register(DeadlineScheduleItemType)
+admin.register(Event)
+admin.register(Deadline)
